5153f1b72b945d295e6307ce505c70f753a6091c
businesszh <businesshow123@gmail.com>
2025-05-31 13:01:36 +0800

    feat: 本地最新改动

--- app/[locale]/(default)/page.tsx
+++ app/[locale]/(default)/page.tsx
@@ -44,7 +44,7 @@ export default async function LandingPage({
       {page.hero && <Hero hero={page.hero} />}
       {page.branding && <Branding section={page.branding} />}
       {page.introduce && <Feature1 section={page.introduce} />}
-      {page.benefit && <Feature2 section={page.benefit} />}
+      {/*{page.benefit && <Feature2 section={page.benefit} />}
       {page.usage && <Feature3 section={page.usage} />}
       {page.feature && <Feature section={page.feature} />}
       {page.showcase && <Showcase section={page.showcase} />}
@@ -52,7 +52,7 @@ export default async function LandingPage({
       {page.pricing && <Pricing pricing={page.pricing} />}
       {page.testimonial && <Testimonial section={page.testimonial} />}
       {page.faq && <FAQ section={page.faq} />}
-      {page.cta && <CTA section={page.cta} />}
+      {page.cta && <CTA section={page.cta} />}*/}
     </>
   );
 }
